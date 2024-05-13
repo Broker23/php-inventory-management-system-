7cfb573dde1ec4f18a0e020fe73b384f7f920031
Broker23 <159134087+Broker23@users.noreply.github.com>
2024-05-13 11:42:42 +0530

    Update db_connect.php

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -4,7 +4,7 @@ $localhost = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "store";
-$store_url = "http://localhost/php-inventory-management-system/";
+$store_url = "https://php-inventory-management-system-nlf4zgi4z.vercel.app/";
 // db connection
 $connect = new mysqli($localhost, $username, $password, $dbname);
 // check connection
